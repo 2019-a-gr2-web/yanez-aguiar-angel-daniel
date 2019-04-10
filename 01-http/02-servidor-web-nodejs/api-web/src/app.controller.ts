9dfd9408a4c99a582dc934faa12159bdabcf8016
AngelDYaAg <41920641+AngelDYaAg@users.noreply.github.com>
2019-04-10 15:59:56 -0500

    Metodos y status

--- 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, HttpCode, Post} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,27 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post()
+  @HttpCode(200)
+  postHello(){
+    return 'hola mundo en post';
+  }
 }
+
+/*@nombreDecorador() // es una funcion
+class usuario{
+  @atributo()
+  atributoPublico; //publico
+  private atributoPrivado;
+  protected atributoProtegido;
+  constructor(@parametro() atributoPublico,@otroParametro() atributoPrivado,@otroOtroParametro() atributoProtegido){
+    this.atributoPublico = atributoPublico;
+    this.atributoPrivado = atributoPrivado;
+    this.atributoProtegido = atributoProtegido;
+  }
+  @metodoA
+  public metodoPublico(){}
+  private metodoPrivado(){}
+  protected metodoPrivado(){}
+}*/
