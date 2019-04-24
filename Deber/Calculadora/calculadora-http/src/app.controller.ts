a3ebc997e3f53d05871c573e2593ebe2bcac4c8f
AngelDYaAg <angel_0930@hotmail.es>
2019-04-23 23:56:23 -0500

    deber 2 cookies

--- Deber/Calculadora/calculadora-http/src/app.controller.ts
+++ Deber/Calculadora/calculadora-http/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Delete, Put, Post, HttpCode, Headers, Body, Response, Query} from '@nestjs/common';
+import { Controller, Get, Delete, Put, Post, HttpCode, Headers, Body, Response, Request, Query} from '@nestjs/common';
 import { AppService } from './app.service';
+import * as Joi from '@hapi/joi';
 
 @Controller('/calculadora')
 export class AppController {
@@ -9,11 +10,12 @@ export class AppController {
   //puerto: 3001
   @Get('/suma')
   @HttpCode(200)
-  suma(@Headers() headers): number {
+  suma(@Headers() headers){
     console.log('Headers:',headers);
     const numero1 = Number(headers.numero1);
     const numero2 = Number(headers.numero2);
     return numero1 + numero2;
+
   }
 
   @Post('/resta')
@@ -44,5 +46,103 @@ export class AppController {
     const numero1 = Number(queryParams.numero1);
     const numero2 = Number(queryParams.numero2);
     return numero1 / numero2;
+  }
+
+
+
+
+
+  //Deber2: Crear una ruta HTTP con el metodo que ustedes queiran y el nombre que ustedes deseen
+
+  @Get('/cookieSuma')
+  @HttpCode(200)
+  cookieSuma(@Headers() headers, @Request() request, @Response() response){
+    const cookies = request.cookies; //json
+    const esquemaValidacionNumero = Joi.object().keys({
+      nombre:Joi.string().required()
+    });
+    const objetoValidacion = {
+      nombre:cookies.nombre
+    };
+    const resultado = Joi.validate(objetoValidacion,esquemaValidacionNumero);
+    if(resultado.error){
+      console.log('Resultado',resultado);
+    }else{
+      console.log('Nombre Valido');
+      console.log('Headers:',headers);
+      const numero1 = Number(headers.numero1);
+      const numero2 = Number(headers.numero2);
+      const result = numero1 + numero2;
+      response.send({nombre:cookies.nombre, resultado:result});
+    }
+  }
+
+  @Post('/cookieResta')
+  @HttpCode(201)
+  cookieResta(@Body() parametrosCuerpo, @Request() request,@Response() response){
+      const cookies = request.cookies; //json
+      const esquemaValidacionNumero = Joi.object().keys({
+        nombre:Joi.string().required()
+      });
+      const objetoValidacion = {
+        nombre:cookies.nombre
+      };
+      const resultado = Joi.validate(objetoValidacion,esquemaValidacionNumero);
+      if(resultado.error){
+        console.log('Resultado',resultado);
+      }else{
+        console.log('Nombre Valido');
+        const numero1 = Number(parametrosCuerpo.numero1);
+        const numero2 = Number(parametrosCuerpo.numero2);
+        const result = numero1 - numero2;
+        response.set('resultado',`${result}`);
+        response.send({nombre:cookies.nombre, resultado:result});
+      }
+  }
+
+  @Put('/cookieMultiplicacion')
+  @HttpCode(202)
+  cookieMultiplicacion(@Query() queryParams, @Request() request, @Response() response){
+    const cookies = request.cookies; //json
+    const esquemaValidacionNumero = Joi.object().keys({
+      nombre:Joi.string().required()
+    });
+    const objetoValidacion = {
+      nombre:cookies.nombre
+    };
+    const resultado = Joi.validate(objetoValidacion,esquemaValidacionNumero);
+    if(resultado.error){
+      console.log('Resultado',resultado);
+    }else{
+      console.log('Nombre Valido');
+      console.log(queryParams);
+      const numero1 = Number(queryParams.numero1);
+      const numero2 = Number(queryParams.numero2);
+      const result = numero1 * numero2;
+      response.send({nombre:cookies.nombre, resultado:result});
+    }
+  }
+
+  @Delete('/cookieDivision')
+  @HttpCode(203)
+  cookieDivision(@Query() queryParams, @Request() request, @Response() response){
+    const cookies = request.cookies; //json
+    const esquemaValidacionNumero = Joi.object().keys({
+      nombre:Joi.string().required()
+    });
+    const objetoValidacion = {
+      nombre:cookies.nombre
+    };
+    const resultado = Joi.validate(objetoValidacion,esquemaValidacionNumero);
+    if(resultado.error){
+      console.log('Resultado',resultado);
+    }else{
+      console.log('Nombre Valido');
+      console.log(queryParams);
+      const numero1 = Number(queryParams.numero1);
+      const numero2 = Number(queryParams.numero2);
+      const result = numero1 / numero2;
+      response.send({nombre:cookies.nombre, resultado:result});
+    }
   }
 }
