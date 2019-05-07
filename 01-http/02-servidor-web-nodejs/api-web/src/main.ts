b4a7c07fd0edb4aa18f6aad3bbae8750c052195a
AngelDYaAg <41920641+AngelDYaAg@users.noreply.github.com>
2019-05-07 15:59:56 -0500

    HTML estructura

--- 01-http/02-servidor-web-nodejs/api-web/src/main.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/main.ts
@@ -2,6 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {NestExpressApplication} from "@nestjs/platform-express";
 import {join} from "path";
+import * as express from 'express';
+import * as path from 'path';
+import * as favicon from 'serve-favicon';
+
 //import * as cookieParser from 'cookieParser';
 var cookieParser = require('cookie-parser');
 
@@ -10,6 +14,8 @@ async function bootstrap() {
   app.use(cookieParser('secreto'));
   app.setViewEngine('ejs');
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.use(express.static('publico'));
+  app.use(favicon(path.join(__dirname,'..', 'publico','imagenes', 'logo-batman.ico')))
   await app.listen(3000);
 }
 bootstrap();
