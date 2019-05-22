7558f007a0f88f68db009ca0a00f72a431218987
AngelDYaAg <41920641+AngelDYaAg@users.noreply.github.com>
2019-05-22 16:00:33 -0500

    formulario crear

--- 01-http/02-servidor-web-nodejs/api-web/src/tragos/tragos.controller.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/tragos/tragos.controller.ts
@@ -1,5 +1,6 @@
-import {Controller, Get, Res} from "@nestjs/common";
+import {Body, Controller, Get, Post, Res} from "@nestjs/common";
 import {TragosService} from "./tragos.service";
+import {Trago} from "./interfaces/trago";
 
 @Controller('/api/traguito')
 export class TragosController {
@@ -16,4 +17,53 @@ export class TragosController {
         })
 }
 
+    @Get('crear')
+    crearTrago(@Res() res){
+        res.render('tragos/crear-editar')
+    }
+
+    @Post('crear')
+    crearTragoPost(@Body() trago: Trago,
+                   @Res() res
+                   // @Body('nombre') nombre:string,
+                   // @Body('tipo') tipo:string,
+                   // @Body('gradosAlcohol') gradosAlcohol:number,
+                   // @Body('fechaCaducidad') fechaCaducidad:Date,
+                   // @Body('precio') precio:number,
+){
+        trago.gradosAlcohol = Number(trago.gradosAlcohol);
+        trago.precio = Number(trago.precio);
+        trago.fechaCaducidad = new Date(trago.fechaCaducidad);
+        console.log(trago);
+
+        this._tragosService.crear(trago);
+        res.redirect('/api/traguito/lista')
+
+
+        // console.log('Nombre: ', nombre, typeof nombre);
+        // console.log('Tipo: ', tipo, typeof tipo);
+        // console.log('GradosAlcohol: ', gradosAlcohol, typeof gradosAlcohol);
+        // console.log('FechaCaducidad: ', fechaCaducidad, typeof fechaCaducidad);
+        // console.log('Precio: ', precio, typeof precio);
+    }
+
+    @Post('eliminar')
+    eliminarTragoPost(@Body() id: number,
+                   @Res() res
+                   // @Body('nombre') nombre:string,
+                   // @Body('tipo') tipo:string,
+                   // @Body('gradosAlcohol') gradosAlcohol:number,
+                   // @Body('fechaCaducidad') fechaCaducidad:Date,
+                   // @Body('precio') precio:number,
+    ){
+
+        res.redirect('/api/traguito/lista')
+
+
+        // console.log('Nombre: ', nombre, typeof nombre);
+        // console.log('Tipo: ', tipo, typeof tipo);
+        // console.log('GradosAlcohol: ', gradosAlcohol, typeof gradosAlcohol);
+        // console.log('FechaCaducidad: ', fechaCaducidad, typeof fechaCaducidad);
+        // console.log('Precio: ', precio, typeof precio);
+    }
 }
