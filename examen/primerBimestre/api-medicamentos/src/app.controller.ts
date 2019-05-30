3b214f7765fddc9a0fb53e65220416ba3e78bebf
AngelDYaAg <41920641+AngelDYaAg@users.noreply.github.com>
2019-05-30 12:12:14 -0500

    examen vistas

--- examen/primerBimestre/api-medicamentos/src/app.controller.ts
+++ examen/primerBimestre/api-medicamentos/src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Res, Post, Body} from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/api/examen')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('login')
+  login(@Res() res){
+    res.render('login')
+  }
+
+  @Get('principal')
+  principal(@Res() res){
+    res.render('pantallaPrincipal')
+  }
+
+  @Get('paciente')
+  paciente(@Res() res){
+    res.render('gestionarPapas')
+  }
+
+  @Get('crearPaciente')
+  crearPaciente(@Res() res){
+    res.render('crearPapa')
+  }
+
+  @Post('principal')
+  principalPost(@Body() nombre: string,
+                    @Res() res
+  ){
+    //this._tragosService.eliminarPorId(id);
+    res.redirect('/api/examen/principal')
   }
 }
--- examen/primerBimestre/api-medicamentos/src/main.ts
+++ examen/primerBimestre/api-medicamentos/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {NestExpressApplication} from "@nestjs/platform-express";
+import * as express from 'express';
+import * as path from 'path';
+import {join} from "path";
+var cookieParser = require('cookie-parser');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)as NestExpressApplication;
+  app.use(cookieParser('secreto'));
+  app.setViewEngine('ejs');
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.use(express.static('publico'));
   await app.listen(3000);
 }
 bootstrap();
