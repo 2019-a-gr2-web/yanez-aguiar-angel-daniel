05ce16c512da79a7e2b9b41c312d1e36a8d16cff
AngelDYaAg <41920641+AngelDYaAg@users.noreply.github.com>
2019-06-12 16:01:18 -0500

    base de datos

--- 01-http/02-servidor-web-nodejs/api-web/src/app.module.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {TragosModules} from "./tragos/tragos.module";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {TragosEntity} from "./tragos/tragos.entity";
 
 @Module({
-  imports: [TragosModules],
+  imports: [TragosModules,TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'test',
+    entities: [TragosEntity],
+    synchronize: true
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
